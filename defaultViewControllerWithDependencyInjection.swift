222052fc69d3e14dd210e02895c05bb31eb4f7fe
Raghav Mangrola <raghav.mangrola@eventfarm.com>
2017-06-29 15:59:49 -0700

    Default View Controller with Dependency Injection

---
+++ defaultViewControllerWithDependencyInjection.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+class <#Class Name#>ViewController: UIViewController {
+
+    // MARK: - Enums
+
+    // MARK: - Constants
+
+    // MARK: - IBOutlets
+
+    // MARK: - Weak Vars
+
+    // MARK: - Public Variables
+
+    // MARK: - Private Variables
+
+    // MARK: - Init
+
+    init() {
+        super.init(nibName: "<#Nib Name#>", bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) is not supported")
+    }
+
+    // MARK: - View Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+
+    // MARK: - IBActions
+
+    // MARK: - Instance Methods
+
+}
